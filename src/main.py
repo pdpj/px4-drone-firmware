a46d80eef637f5035af2e4eef3deacc34b5c5f9d
pdpj <65965305+pdpj@users.noreply.github.com>
2024-06-19 21:23:55 +0100

    Update main.py
    pressione x ou y para fazer ações com o drone

--- src/main.py
+++ src/main.py
@@ -1,9 +1,27 @@
-#main.py
 import asyncio
-from utils.mission_orbit import function1
+import sys
+from teste import function1, function2
 
 async def main():
     await function1()
 
+async def main2():
+    await function2()    
+
+async def wait_loop():
+    print("Pressione 'x' para calculo de bateria ou 'y' para missao")
+    while True:
+        key = await get_keypress()
+        if key == 'x':
+            await main()
+        elif key == 'y':
+            await main2()
+
+async def get_keypress():
+    loop = asyncio.get_event_loop()
+    future = loop.run_in_executor(None, sys.stdin.read, 1)
+    key = await future
+    return key.strip().lower()
+
 if __name__ == "__main__":
-    asyncio.run(main())
+    asyncio.run(wait_loop())
